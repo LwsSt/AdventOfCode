bc98e25afd6e6618c79d1aabeff8eaf3195d13d5
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 20:27:20 +0000

    Implemented part 2

--- y2017/d01/Day01.fs
+++ y2017/d01/Day01.fs
@@ -1,11 +1,21 @@
 module Day01
+let inline charToInt (c: char) = int c - int '0'
 
 let captcha (str: string) : int =
     let input = str + (string (str.Chars 0))
-    let inline charToInt c = int c - int '0'
     input
     |> Seq.map charToInt
     |> Seq.pairwise
+    |> Seq.filter (fun (x, y) -> x = y)
+    |> Seq.map fst
+    |> Seq.sum
+
+let captcha2 (str: string) : int = 
+    let length = String.length str
+    let input = Seq.map charToInt str
+    let inputOffset = str.Substring(length/2) + str |> Seq.map charToInt
+    input
+    |> Seq.zip inputOffset
     |> Seq.filter (fun (x, y) -> x = y)
     |> Seq.map fst
     |> Seq.sum
