d392e44c404b50d48e521cb5719a198a2310326d
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 14:28:58 +0000

    test name describes part

--- y2017/d01/Day01.Tests.fs
+++ y2017/d01/Day01.Tests.fs
@@ -5,17 +5,17 @@ open Xunit
 open FsUnit.Xunit
 
 [<Fact>]
-let ``1122 returns 3`` () =
+let ``[Part 1] 1122 returns 3`` () =
     captcha "1122" |> should equal 3
 
 [<Fact>]
-let ``1111 returns 4`` () =
+let ``[Part 1] 1111 returns 4`` () =
     captcha "1111" |> should equal 4
 
 [<Fact>]
-let ``1234 returns 0`` () =
+let ``[Part 1] 1234 returns 0`` () =
     captcha "1234" |> should equal 0
 
 [<Fact>]
-let ``91212129 returns 9`` () =
+let ``[Part 1] 91212129 returns 9`` () =
     captcha "91212129" |> should equal 9
