d4644b0de513c394de0711ade083a36ec6fd5d48
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 19:31:35 +0000

    Refactor part 1 to be simpler

--- y2017/d01/Day01.fs
+++ y2017/d01/Day01.fs
@@ -3,12 +3,9 @@ module Day01
 let captcha (str: string) : int =
     let input = str + (string (str.Chars 0))
     let inline charToInt c = int c - int '0'
-    let reducer (acc: (int * int)) (next: int) = 
-        match acc with
-        | (0, 0) -> (0, next)
-        | (total, prev) when prev = next -> (total + next, next)
-        | (total, _) -> (total, next)
     input
     |> Seq.map charToInt
-    |> Seq.fold reducer (0, 0)
-    |> fst
+    |> Seq.pairwise
+    |> Seq.filter (fun (x, y) -> x = y)
+    |> Seq.map fst
+    |> Seq.sum
