409ec04340f57f16be5db3f14115b49ae6381260
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 14:26:10 +0000

    Implement first pass solution

--- y2017/d01/Day01.fs
+++ y2017/d01/Day01.fs
@@ -1,3 +1,14 @@
 module Day01
 
-let captcha (str: string) : int = failwith "Not implemented"
+let captcha (str: string) : int =
+    let input = str + (string (str.Chars 0))
+    let inline charToInt c = int c - int '0'
+    let reducer (acc: (int * int)) (next: int) = 
+        match acc with
+        | (0, 0) -> (0, next)
+        | (total, prev) when prev = next -> (total + next, next)
+        | (total, _) -> (total, next)
+    input
+    |> Seq.map charToInt
+    |> Seq.fold reducer (0, 0)
+    |> fst
