55b865292b788363f40e92f1fee46d6b298f05e3
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 20:27:07 +0000

    Added tests for part 2

--- y2017/d01/Day01.Tests.fs
+++ y2017/d01/Day01.Tests.fs
@@ -19,3 +19,23 @@ let ``[Part 1] 1234 returns 0`` () =
 [<Fact>]
 let ``[Part 1] 91212129 returns 9`` () =
     captcha "91212129" |> should equal 9
+
+[<Fact>]
+let ``[Part 2] 1212 returns 6`` () =
+    captcha2 "1212" |> should equal 6
+
+[<Fact>]
+let ``[Part 2] 1221 returns 0`` () = 
+    captcha2 "1221" |> should equal 0
+
+[<Fact>]
+let ``[Part 2] 123425 return 4`` () =
+    captcha2 "123425" |> should equal 4
+
+[<Fact>]
+let ``[Part 2] 123123 returns 12`` () =
+    captcha2 "123123" |> should equal 12
+
+[<Fact>]
+let ``[Part 2] 12131415 returns 4`` () =
+    captcha2 "12131415" |> should equal 4
