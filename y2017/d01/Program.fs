85914a395e9f810dbc5321d8fa59fe633d820bc9
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-01 20:29:06 +0000

    main prints part 1 and 2

--- y2017/d01/Program.fs
+++ y2017/d01/Program.fs
@@ -3,7 +3,8 @@ open Day01
 
 [<EntryPoint>]
 let main argv =
-    let puzzleInput = System.IO.File.ReadAllText "puzzle-input-1"
-    let solution = captcha puzzleInput
-    printfn "%d" solution
+    let puzzleInput1 = System.IO.File.ReadAllText "puzzle-input-1"
+    let puzzleInput2 = System.IO.File.ReadAllText "puzzle-input-2"
+    captcha puzzleInput1 |> printfn "Part 1: %d"
+    captcha2 puzzleInput2 |> printfn "Part 2: %d"
     0
