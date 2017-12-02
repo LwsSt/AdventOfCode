c447700e9669c6ed7745de0793d593d436ba6c81
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-02 23:01:31 +0000

    Implemented part 2

--- y2017/d02/Day02.fs
+++ y2017/d02/Day02.fs
@@ -8,3 +8,11 @@ let checksumLine (line: int[]) :int =
 let checksum1 (lines: string[]) :int =
     let tabSplit (s: string) = s.Split [| '\t' |]
     Array.sumBy (tabSplit >> (Array.map int) >> checksumLine) lines
+
+let checksumLine2 (line: int[]) : int = 
+    (Array.allPairs line line |> Array.filter (fun (a, b) -> a <> b && a % b = 0))
+    |> Array.sumBy (fun (a, b) -> a / b)
+
+let checksum2 (lines: string[]) : int =
+    let tabSplit (s: string) = s.Split [| '\t' |]
+    Array.sumBy (tabSplit >> (Array.map int) >> checksumLine2) lines
