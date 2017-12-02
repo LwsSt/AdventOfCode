deb48a011f272f543083c0e4db300caf9b8317da
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-02 19:41:35 +0000

    Refactored in prep for part 2

--- y2017/d02/Day02.fs
+++ y2017/d02/Day02.fs
@@ -1,12 +1,10 @@
 module Day02
 
-let checksumLine (line: string[]) :int =
-    let vals = Array.map int line
-    let max = Array.max vals
-    let min = Array.min vals
+let checksumLine (line: int[]) :int =
+    let max = Array.max line
+    let min = Array.min line
     max - min
-
 
 let checksum1 (lines: string[]) :int =
     let tabSplit (s: string) = s.Split [| '\t' |]
-    Array.sumBy (tabSplit >> checksumLine) lines
+    Array.sumBy (tabSplit >> (Array.map int) >> checksumLine) lines
