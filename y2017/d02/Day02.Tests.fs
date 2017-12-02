3bc43b9e554cad90147d65b7820188552ed43029
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-02 19:26:21 +0000

    Implemented part 1 and test

--- y2017/d02/Day02.Tests.fs
+++ y2017/d02/Day02.Tests.fs
@@ -1,3 +1,12 @@
 module Day02Tests
 
 open Day02
+open FsUnit
+open Xunit
+
+let testInput = [|"5\t1\t9\t5"; "7\t5\t3"; "2\t4\t6\t8"|]
+
+[<Fact>]
+let ``[Part 1] Test input returns 18`` ()=
+    checksum1 testInput 
+    |> should equal 18
--- y2017/d02/Day02.fs
+++ y2017/d02/Day02.fs
@@ -1,1 +1,12 @@
 module Day02
+
+let checksumLine (line: string[]) :int =
+    let vals = Array.map int line
+    let max = Array.max vals
+    let min = Array.min vals
+    max - min
+
+
+let checksum1 (lines: string[]) :int =
+    let tabSplit (s: string) = s.Split [| '\t' |]
+    Array.sumBy (tabSplit >> checksumLine) lines
