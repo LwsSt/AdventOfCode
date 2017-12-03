5bf7a25781c5930fa9cf2fa49c649d2574e44b22
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-03 19:30:16 +0000

    Added [<Fact>] to test

--- y2017/d03/Day03.Tests.fs
+++ y2017/d03/Day03.Tests.fs
@@ -4,14 +4,18 @@ open Day03
 open FsUnit
 open Xunit
 
+[<Fact>]
 let ``[Part 1] 1 returns 0 steps`` ()=
     memory 1 |> should equal 0
 
+[<Fact>]
 let ``[Part 1] 12 returns 3 steps`` ()=
     memory 12 |> should equal 3
 
+[<Fact>]
 let ``[Part 1] 23 returns 2 steps`` ()=
     memory 23 |> should equal 2
 
+[<Fact>]
 let ``[Part 1] 1024 returns 31 steps`` ()=
     memory 1025 |> should equal 31
