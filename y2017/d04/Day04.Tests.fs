64d375f2db22fe5b18d31f5eb6ed3388ed87faac
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-04 09:17:23 +0000

    Fixed test inputs

--- y2017/d04/Day04.Tests.fs
+++ y2017/d04/Day04.Tests.fs
@@ -9,8 +9,8 @@ let ``[Part 1] "aa bb cc dd ee" is a valid passphrase`` ()=
     isValid "aa bb cc dd ee" |> should be True
 
 [<Fact>]
-let ``[Part 1] "aa bb cc dd ee aa" is not a valid passphrase`` ()=
-    isValid "aa bb cc dd ee" |> should be False
+let ``[Part 1] "aa bb cc dd aa" is not a valid passphrase`` ()=
+    isValid "aa bb cc dd aa" |> should be False
    
 [<Fact>]
 let ``[Part 1] "aa bb cc dd aaa" is a valid passphrase`` ()=
