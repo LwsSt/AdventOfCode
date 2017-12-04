d9f24c1b6e7a31de35b433771bb0b82bc929c4eb
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-04 09:22:11 +0000

    Implemented Part 1 Day04

--- y2017/d04/Day04.fs
+++ y2017/d04/Day04.fs
@@ -1,3 +1,7 @@
 module Day04
 
-let isValid (str: string): bool = failwith "Implement"
+let isValid (str: string): bool =
+    let words = str.Split [|' '|]
+    let wordCount = words.Length
+    let distinctWordCount = words |> Array.distinct |> Array.length
+    wordCount = distinctWordCount
