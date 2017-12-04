608d0aeea1b4687667c0b2a398f9f7bfd96e429e
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-04 13:38:46 +0000

    Implemented Part 2

--- y2017/d04/Day04.fs
+++ y2017/d04/Day04.fs
@@ -6,4 +6,13 @@ let isValid (str: string): bool =
     let distinctWordCount = words |> Array.distinct |> Array.length
     wordCount = distinctWordCount
 
-let isValid2 (str: string) : bool = failwith "Implement"
+let isAnagram (a: string, b: string) = 
+    let sort (s: string): string = s |> Seq.sort |> System.String.Concat
+    a.Length = b.Length && (sort a) = (sort b)
+
+let isValid2 (str: string) : bool = 
+    let words = str.Split [|' '|] |> Array.toSeq
+    Seq.allPairs words words
+    |> Seq.filter (fun (a, b) -> a <> b)
+    |> Seq.exists isAnagram
+    |> not
