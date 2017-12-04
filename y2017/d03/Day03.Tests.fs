af73c18cef0c23ab83693abe02198f681dbd0bb1
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-04 22:14:23 +0000

    Added tests for initial values

--- y2017/d03/Day03.Tests.fs
+++ y2017/d03/Day03.Tests.fs
@@ -9,6 +9,18 @@ let ``[Part 1] 1 returns 0 steps`` ()=
     memory 1 |> should equal 0
 
 [<Fact>]
+let ``[Part 1] 2 returns 1 steps`` ()=
+    memory 2 |> should equal 1
+
+[<Fact>]
+let ``[Part 1] 3 returns 2 steps`` ()=
+    memory 3 |> should equal 2
+
+[<Fact>]
+let ``[Part 1] 4 returns 1 steps`` ()=
+    memory 4 |> should equal 1
+
+[<Fact>]
 let ``[Part 1] 12 returns 3 steps`` ()=
     memory 12 |> should equal 3
 
