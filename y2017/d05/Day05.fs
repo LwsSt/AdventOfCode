cec6b8764a3615fde01f074f898931531f18a266
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-05 09:27:10 +0000

    Implemented Part 02

--- y2017/d05/Day05.fs
+++ y2017/d05/Day05.fs
@@ -1,11 +1,22 @@
 module Day05
 
-let escape (input: int[]) : int = 
+let escape (input: int[]): int = 
     let mutable ptr = 0
     let mutable steps = 0
     while ptr < input.Length do
         let jump = input.[ptr]
         input.[ptr] <- input.[ptr] + 1
+        ptr <- ptr + jump
+        steps <- steps + 1
+    steps
+
+let escape2 (input: int[]): int = 
+    let nextVal i = if i >= 3 then i - 1 else i + 1
+    let mutable ptr = 0
+    let mutable steps = 0
+    while ptr < input.Length do
+        let jump = input.[ptr]
+        input.[ptr] <- nextVal input.[ptr]
         ptr <- ptr + jump
         steps <- steps + 1
     steps
@@ -16,5 +27,9 @@ module Tests =
     open Xunit
 
     [<Fact>]
-    let ``Example input takes 5 steps`` ()=
+    let ``[Part 1] Example input takes 5 steps`` ()=
         escape [|0; 3; 0; 1; -3|] |> should equal 5
+
+    [<Fact>]
+    let ``[Part 2] Example input takes 10 steps`` ()=
+        escape2 [|2; 3; 2; 3; -1|]
