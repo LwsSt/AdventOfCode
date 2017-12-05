d32fa3df9bc64db3bae4a8325f67a8c529860646
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-05 09:28:34 +0000

    main prints Part 1 and Part 2

--- y2017/d05/Program.fs
+++ y2017/d05/Program.fs
@@ -6,9 +6,10 @@ open Day05
 [<EntryPoint>]
 let main argv =
     let puzzleInput = System.IO.File.ReadAllLines "puzzle-input"
-    puzzleInput
-    |> Seq.map int
-    |> Seq.toArray
-    |> escape
-    |> printfn "Part 1: %d"
+    let instructions = 
+        puzzleInput
+        |> Seq.map int
+        |> Seq.toArray
+    escape instructions |> printfn "Part 1: %d"
+    escape2 instructions |> printfn "Part 2: %d"
     0 // return an integer exit code
