4948aaa8dd46f97fa3ce5dd59d5d7a3a22d98247
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-06 20:42:14 +0000

    Implemented Day 06 Part 1

--- y2017/d06/Day06.fs
+++ y2017/d06/Day06.fs
@@ -1,8 +1,25 @@
 module Day06
 
-let cycle (input: int list): int list = failwith "Implement"
+let cycle (input: int list): int list = 
+    let length = List.length input
+    let max = List.max input
+    let maxIdx = input |> List.findIndex (fun x -> x = max)
+    let inputArr = input |> List.toArray
+    inputArr.[maxIdx] <- 0
+    let rec cycleImpl (arr: int[]) index allocs = 
+        if allocs = 0 then arr else
+        arr.[index] <- arr.[index] + 1
+        cycleImpl arr ((index + 1) % length) (allocs - 1)
+    cycleImpl inputArr ((maxIdx + 1) % length) max
+    |> List.ofArray
 
-let escape (input: int list): int = failwith "Implement"
+let escape (input: int list): int =
+    let rec escapeImpl coll count alloc =
+        let nextCycle = cycle alloc
+        match List.contains nextCycle coll with
+        | true -> count + 1
+        | false -> escapeImpl (nextCycle :: coll) (count + 1) nextCycle
+    escapeImpl [] 0 input
 
 module Test = 
 
