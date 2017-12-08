ae3013f02b556a429ac121518a81cab81f25afc4
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-08 22:12:05 +0000

    Implemented line parsing

--- y2017/d07/Day07.fs
+++ y2017/d07/Day07.fs
@@ -8,10 +8,24 @@ type ProgramInfo = {
     programs: string list
 }
 
+let construct n w ps = {name= n; weight= w; programs= ps}
+
 type Tree =
     | Leaf of string * int
     | Node of string * int * Tree list
 
-let parseLine (str: string): ProgramInfo = failwith "Implement"
+let parseLine (str: string): ProgramInfo = 
+    let combine n w ops =
+        match ops with
+        | Some ps -> construct n w ps
+        | None -> construct n w []
+    let name = many1Chars letter
+    let weight = between (pstring "(") (pstring ")") pint32 |>> int
+    let programs = opt (spaces1 >>. pstring "->" >>. spaces1 >>. sepBy name (pstring "," >>. spaces))
+    let parseLineImpl = pipe3 (name .>> spaces) weight programs combine
+    match run parseLineImpl str with
+    | Success (info, _, _) -> info
+    | Failure (err, _, _) -> failwith err
+
 
 let towers (str: string list): string = failwith "Implement"
