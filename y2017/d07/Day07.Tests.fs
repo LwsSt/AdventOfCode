c904b2c512b6b3906be90329f56bd45501e9c71d
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-08 21:17:21 +0000

    Set up for Day 07

---
+++ y2017/d07/Day07.Tests.fs
@@ -0,0 +1,24 @@
+module Day07Tests
+
+open Day07
+open FsUnit
+open Xunit
+
+let testInput = [
+    "pbga (66)"
+    "xhth (57)"
+    "ebii (61)"
+    "havc (66)"
+    "ktlj (57)"
+    "fwft (72) -> ktlj, cntj, xhth"
+    "qoyq (66)"
+    "padx (45) -> pbga, havc, qoyq"
+    "tknk (41) -> ugml, padx, fwft"
+    "jptl (61)"
+    "ugml (68) -> gyxo, ebii, jptl"
+    "gyxo (61)"
+    "cntj (57)"
+]
+
+let ``[Part 1] Test input returns tknk`` ()=
+    towers testInput |> should equal "tknk"
---
+++ y2017/d07/Day07.fs
@@ -0,0 +1,3 @@
+module Day07
+
+let towers (str: string list): string = failwith "Implement"
