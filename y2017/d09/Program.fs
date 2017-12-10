745747bf63228d5426091881e00fee9d0202e356
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-10 12:58:34 +0000

    main prints Part 2 Day 09

--- y2017/d09/Program.fs
+++ y2017/d09/Program.fs
@@ -5,4 +5,6 @@ let main argv =
     let puzzleInput = System.IO.File.ReadAllText "puzzle-input"
     score puzzleInput
     |> printfn "Part 1: %d"
+    score2 puzzleInput
+    |> printfn "Part 2: %d"
     0
