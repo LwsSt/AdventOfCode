0ae7c449028864b7255a0550eb354485ebfde5a5
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-11 13:19:34 +0000

    main now prints Part 2 Day 10

--- y2017/d10/Program.fs
+++ y2017/d10/Program.fs
@@ -3,7 +3,10 @@
 [<EntryPoint>]
 let main argv =
     let puzzleInput1 = [31; 2; 85; 1; 80; 109; 35; 63; 98; 255; 0; 13; 105; 254; 128; 33]
+    let puzzleInput2 = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"
     let input = Seq.init 256 id |> Seq.toList
     hash1 puzzleInput1 input
     |> printfn "Part 1: %d"
+    hash2 puzzleInput2 input
+    |> printfn "Part 2: %s"
     0
