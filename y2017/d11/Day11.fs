b7a4fd239aa7566223c6b1760c91e3b0b51470b0
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-11 20:48:39 +0000

    Implemented Part 2 Day 11

--- y2017/d11/Day11.fs
+++ y2017/d11/Day11.fs
@@ -1,20 +1,55 @@
 module Day11
 
-let traverse (moves: string seq): int = failwith "Implement"
+let n (a, b) = (a, b - 1)
+let s (a, b) = (a, b + 1)
+let nw (a, b) = (a - 1, b)
+let se (a, b) = (a + 1, b)
+let ne = n >> se
+let sw = s >> nw
+
+let parse = function
+| "n" -> n
+| "s" -> s
+| "ne" -> ne
+| "nw" -> nw
+| "sw" -> sw
+| "se" -> se
+| _ -> failwith "Invalid direction"
+
+let toCube (x, y) = (x, y, (-x) - y)
+
+let cubeDistance (x1, y1, z1) (x2, y2, z2)=
+    let dx = abs (x1 - x2)
+    let dy = abs (y1 - y2)
+    let dz = abs (z1 - z2)
+    max dx dy |> max dz
+
+let traverse (moves: string seq): int = 
+    let moveFunc = 
+        moves
+        |> Seq.map parse
+        |> Seq.reduce (>>)
+    moveFunc (0, 0)
+    |> toCube
+    |> cubeDistance (0, 0, 0)
 
 module Tests = 
 
     open FsUnit
     open Xunit
    
+    [<Fact>]
     let ``ne,ne,ne is 3 steps away`` ()=
         traverse ["ne"; "ne"; "ne"] |> should equal 3
 
+    [<Fact>]
     let ``ne,ne,sw,sw is 0 steps away`` ()=
         traverse ["ne"; "ne"; "sw"; "sw"] |> should equal 0
 
+    [<Fact>]
     let ``ne,ne,s,s is 2 steps away`` ()=
         traverse ["ne"; "ne"; "s"; "s"] |> should equal 2
 
+    [<Fact>]
     let ``se,sw,se,sw,sw is 3 steps away`` ()=
         traverse ["se"; "sw"; "se"; "sw"; "sw"] |> should equal 3
