03effc5ffddf1b9f247fada923d93374be629990
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-12 12:12:21 +0000

    Added unit tests for parsing

--- y2017/d12/Day12.fs
+++ y2017/d12/Day12.fs
@@ -22,3 +22,14 @@ module Tests =
     [<Fact>]
     let ``Test input returns 6 programs`` ()=
         plumb testInput |> should equal 6
+
+    [<Fact>]
+    let ``Parse "0 <-> 2" returns [(0, 2), (2, 0)]`` ()=
+        parse "0 <-> 2" |> should equal ([(0, 2), (2, 0)] |> Set.ofList)
+
+    [<Fact>]
+    let ``Parse "2 <-> 0, 3, 4" returns [(2, 0), (0, 2), (2, 3), (3, 2), (2, 4), (4, 2)]`` ()=
+        parse "2 <-> 0, 3, 4" |> should equal ([(2, 0), (0, 2), (2, 3), (3, 2), (2, 4), (4, 2)] |> Set.ofList)
+
+    let ``Parse "1 <-> 1 returns [(1, 1)]`` ()=
+        parse "1 <-> 1" |> should equal ([(1, 1)] |> Set.ofList)
