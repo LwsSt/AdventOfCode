baf675fc283734184cd9c2942a050867682986ae
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-12 16:46:15 +0000

    main prints Part 1 Day 12

--- y2017/d12/Program.fs
+++ y2017/d12/Program.fs
@@ -1,8 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open Day12
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    System.IO.File.ReadLines "puzzle-input"
+    |> plumb
+    |> printfn "Part 1: %d"
+    0
