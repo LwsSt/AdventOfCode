ae052cff0c77b1d1450187184d4581ec0413c63b
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-14 11:43:04 +0000

    Implemented Part 1 Day 14

--- y2017/d14/Day14.fs
+++ y2017/d14/Day14.fs
@@ -1,6 +1,41 @@
 module Day14
 
-let defrag (str: string): int = failwith "Implement"
+open KnotHash
+
+let hash s = knotHash s (Seq.init 256 id |> Seq.toList)
+
+let lookup = [
+    ( '0', "0000" )
+    ( '1', "0001" )
+    ( '2', "0010" )
+    ( '3', "0011" )
+    ( '4', "0100" )
+    ( '5', "0101" )
+    ( '6', "0110" )
+    ( '7', "0111" )
+    ( '8', "1000" )
+    ( '9', "1001" )
+    ( 'a', "1010" )
+    ( 'b', "1011" )
+    ( 'c', "1100" )
+    ( 'd', "1101" )
+    ( 'e', "1110" )
+    ( 'f', "1111" )] |> Map.ofSeq
+
+let hexToBinary (str: string) =
+    str
+    |> Seq.map (fun s -> Map.find s lookup)
+    |> Seq.reduce (+)
+    
+
+let genRows str = Seq.init 128 (fun n -> sprintf "%s-%d" str n) |> Seq.toList
+
+let defrag (str: string): int = 
+    str
+    |> genRows
+    |> Seq.map (hash >> hexToBinary)
+    |> Seq.collect (fun s -> s |> Seq.filter ((=) '1'))
+    |> Seq.length
 
 module Tests = 
     open FsUnit
@@ -8,4 +43,4 @@ module Tests =
 
     [<Fact>]
     let ``flqrgnkx has 8108 used square`` ()=
-        defrag "flqrgnkx" |> should equal 128
+        defrag "flqrgnkx" |> should equal 8108
