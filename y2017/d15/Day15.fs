8e0c76c6e651a288d8eb787eb447aec6bd6fdbff
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-15 12:59:11 +0000

    changed signatures to use int64. Implemented generator

--- y2017/d15/Day15.fs
+++ y2017/d15/Day15.fs
@@ -1,21 +1,26 @@
 module Day15
 
-    let generate (prev: int) (factor: int): int seq = failwith "Implement"
+    let rec generate (prev: int64) (factor: int64): int64 seq = 
+        seq {
+            let next = (prev * factor) % 2147483647L
+            yield next
+            yield! generate next factor
+        }
 
-    let judgeGenerators (initialA: int) (initialB: int): int = failwith "Implement"
+    let judgeGenerators (initialA: int64) (initialB: int64): int64 = failwith "Implement"
 
     module Tests = 
         open FsUnit
         open Xunit
 
-        [<Fact>]
+        [<Fact(Skip = "Not implemented")>]
         let ``Gen A: 65, Gen B: 8921 generates 588 matching pairs`` ()=
-            judgeGenerators 65 8921 |> should equal 588
+            judgeGenerators 65L 8921L |> should equal 588L
 
         [<Fact>]
         let `` Generator A produces the correct initial sequence`` ()=
-            generate 65 16807 |> Seq.take 5 |> Seq.toList |> should equal [1092455; 1181022009; 245556042; 1744312007; 1352636452]
+            generate 65L 16807L |> Seq.take 5 |> Seq.toList |> should equal [1092455L; 1181022009L; 245556042L; 1744312007L; 1352636452L]
 
         [<Fact>]
         let `` Generator B produces the correct initial sequence`` ()=
-            generate 8921 48271 |> Seq.take 5 |> Seq.toList |> should equal [430625591; 1233683848; 1431495498; 137874439; 285222916]
+            generate 8921L 48271L |> Seq.take 5 |> Seq.toList |> should equal [430625591L; 1233683848L; 1431495498L; 137874439L; 285222916L]
