809fcf73eaa5ca3716cb02da5d1af55fb1544e77
Lewis Stapleton <lewis.stapleton@outlook.com>
2017-12-18 13:51:44 +0000

    Added types. Added tests for Parsing

--- y2017/d18/Day18.fs
+++ y2017/d18/Day18.fs
@@ -1,5 +1,21 @@
 module Day18
 
+type Arg = 
+    | Register of char
+    | Number of int
+
+type Instruction =
+    | Send of char
+    | Set of char * Arg
+    | Add of char * Arg
+    | Mul of char * Arg
+    | Mod of char * Arg
+    | Rec of char
+    | Jump of Arg * Arg
+
+let parse (str: string): Instruction = failwith "Implement"
+
+
 let run (program: string list): int = failwith "Implement"
 
 module Tests =
@@ -21,5 +37,37 @@ module Tests =
     ]
 
     [<Fact>]
-    let ``Test input produces value of 4`` ()=
+    let ``[Part 1] Test input produces value of 4`` ()=
         run testInput |> should equal 4
+
+    [<Fact>]
+    let ``Parse: set a 1 -> Set('a', Number 1)`` ()=
+        parse "set a 1" |> should equal <| Set ('a', Number 1)
+
+    [<Fact>]
+    let ``Parse: add a 2 -> Add('a', Number 2)`` ()=
+        parse "add a 2" |> should equal <| Add ('a', Number 2)
+
+    [<Fact>]
+    let ``Parse: mul a a -> Mul('a' Register 'a')`` ()=
+        parse "mul a a" |> should equal <| Mul ('a', Register 'a')
+
+    [<Fact>]
+    let ``Parse: mod a 5 -> Mod('a', Number 5)`` ()=
+        parse "mod a 5" |> should equal <| Mod ('a', Number 5)
+
+    [<Fact>]
+    let ``Parse: snd a -> Send 'a'`` ()=
+        parse "snd a" |> should equal <| Send 'a'
+
+    [<Fact>]
+    let ``Parse: rcv a -> Rec 'a'`` ()=
+        parse "rcv a" |> should equal <| Rec 'a'
+
+    [<Fact>]
+    let ``Parse: jgz a -2 -> Jump (Register 'a', Number -2)`` ()=
+        parse "jgz a -2" |> should equal <| Jump (Register 'a', Number 2)
+
+    [<Fact>]
+    let ``Parse: jgz 1 2 -> Jump (Number 1, Number 2)`` ()=
+        parse "jgz 1 2" |> should equal <| Jump (Number 1, Number 2)
