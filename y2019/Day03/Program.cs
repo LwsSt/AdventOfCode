cad250ea07124be32f98d7ab5720cdad488cdbad
Lewis <lewis.stapleton@outlook.com>
2019-12-03 21:27:37 +0000

    Implement helper classes

--- y2019/Day03/Program.cs
+++ y2019/Day03/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,5 +11,44 @@ namespace AOC2019.Day03
         {
 
         }
+    }
+
+    class Wire
+    {
+        private readonly Point start;
+        private readonly Point end;
+
+        public Wire(Point start, Point end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public IEnumerable<Point> GetPoints()
+        {
+            yield return start;
+        }
+    }
+
+    struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null &&
+                obj is Point that &&
+                that.X == this.X &&
+                that.Y == this.Y;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(X, Y);
     }
 }
