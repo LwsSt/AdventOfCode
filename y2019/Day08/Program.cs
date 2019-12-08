6851e389a5dabe3b749c0c32f6c2b4c4bfe1c912
Lewis <lewis.stapleton@outlook.com>
2019-12-08 18:23:09 +0000

    2019 Day08 part 2

--- y2019/Day08/Program.cs
+++ y2019/Day08/Program.cs
@@ -15,21 +15,63 @@ namespace AOC2019.Day08
                 .Select(c => c - '0')
                 .ToArray();
             
-            int output = Solve(input, 6, 25);
-            Console.WriteLine("Output: {0}", output);
+            // int output = Solve(input, 6, 25);
+            // Console.WriteLine("Output: {0}", output);
+
+            //Part2Example();
+
+            var output = CombineLayers(input, 6, 25);
+            Print(output);
         }
 
-        static int Solve(int[] input, int height, int width)
+        static void Part2Example()
         {
+            int[] input = "0222112222120000".Select(c => c - '0').ToArray();
+            int[,] output = CombineLayers(input, 2, 2);
+
+            Print(output);
+        }
+
+        static int[,] CombineLayers(int[] input, int height, int width)
+        {
+            int[,] output = new int[height, width];
             var layers = ProcessInput(input, height, width).ToArray();
 
-            int fewestZeros = FindLayer(layers);
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int[,] pixel = layers.Where(l => l[h, w] != 2).First();
+                    output[h, w] = pixel[h, w];
+                }
+            }
 
-            int ones = layers[fewestZeros][1];
-            int twos = layers[fewestZeros][2];
-
-            return ones * twos;
+            return output;
         }
+
+        static void Print(int[,] output)
+        {
+            for (int h = 0; h < output.GetLength(0); h++)
+            {
+                for (int w = 0; w < output.GetLength(1); w++)
+                {
+                    Console.Write(output[h, w].ToString());
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // static int Solve(int[] input, int height, int width)
+        // {
+        //     var layers = ProcessInput(input, height, width).ToArray();
+
+        //     int fewestZeros = FindLayer(layers);
+
+        //     int ones = layers[fewestZeros][1];
+        //     int twos = layers[fewestZeros][2];
+
+        //     return ones * twos;
+        // }
 
         static int FindLayer(int[][] layers)
         {
@@ -40,22 +82,25 @@ namespace AOC2019.Day08
                 .First().layer;
         }
 
-        static IEnumerable<int[]> ProcessInput(int[] input, int height, int width)
+        static IEnumerable<int[,]> ProcessInput(int[] input, int height, int width)
         {
             int length = (height * width);
             int idx = 0;
 
             while (idx < input.Length)
             {
-                int[] dict = new int[3];
                 int end = idx + length;
-                for (; idx < end && idx < input.Length; idx++)
+                var layer = new int[height, width];
+                for (int i = 0; i < height && idx < end; i++)
                 {
-                    int pointer = input[idx];
-                    dict[pointer]++;
+                    for (int j = 0; j < width && idx < end; j++)
+                    {
+                        layer[i, j] = input[idx];
+                        idx++;
+                    }
                 }
 
-                yield return dict;
+                yield return layer;
             }
         }
     }
