5a49f7ffd8db3de99d467aea1714093a9d882225
Lewis <lewis.stapleton@outlook.com>
2019-12-14 21:41:20 +0000

    2019 Day12 part 2

--- y2019/Day12/Program.cs
+++ y2019/Day12/Program.cs
@@ -10,37 +10,28 @@ namespace AOC2019.Day12
     class Program
     {
         public static void Main(string[] args)
+        // public static void Main(string[] args)
         {
             string input = File.ReadAllText(@"Day12\input.txt");
             var planets = Parse(input).ToArray();
             
-            Console.WriteLine("Running simulation...");
-            var step = RunSimulation(planets).Skip(999).First();
-            Console.WriteLine("Total energy {0}", CalculateTotalEnergy(step));
+            // Console.WriteLine("Running simulation...");
+            // var step = RunSimulation(planets).Skip(999).First();
+            // Console.WriteLine("Total energy {0}", CalculateTotalEnergy(step));
+
+            long repetition = FindRepetition(() => RunSimulation(Parse(input).ToArray()));
+            Console.WriteLine("Number of repetitions {0}", repetition * 2);
 
             // PrintPart1Examples();
+            // PrintPart2Examples();
         }
 
         static void PrintPart1Examples()
         {
-            string input1 = @"<x=-1, y=0, z=2>
-<x=2, y=-10, z=-7>
-<x=4, y=-8, z=8>
-<x=3, y=5, z=-1>";
-
-            // var planets = Parse(input).ToArray();
-            // var simulation = RunSimulation(planets).Take(11);
-            // foreach (var step in simulation)
-            // {
-            //     Console.WriteLine("Step");
-            //     foreach (var planet in step)
-            //     {
-            //         Console.WriteLine(planet);
-            //     }
-
-            //     Console.WriteLine("Total Energy {0}", CalculateTotalEnergy(step));
-            //     Console.WriteLine();
-            // }
+//             string input1 = @"<x=-1, y=0, z=2>
+// <x=2, y=-10, z=-7>
+// <x=4, y=-8, z=8>
+// <x=3, y=5, z=-1>";
 
             string input2 = @"<x=-8, y=-10, z=0>
 <x=5, y=5, z=10>
@@ -51,6 +42,80 @@ namespace AOC2019.Day12
             foreach (var step in afterStep99)
             {
                 Console.WriteLine("Total Energy {0, -5}", CalculateTotalEnergy(step));
+            }
+        }
+
+        static void PrintPart2Examples()
+        {
+            var testInputs = new[]
+            {
+                @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>",
+                @"<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>",
+            };
+
+            foreach (var input in testInputs)
+            {
+                long repetition = FindRepetition(() => RunSimulation(Parse(input).ToArray()));
+                Console.WriteLine("Number of repetitions {0}", repetition * 2);
+            }
+        }
+
+        static long FindRepetition(Func<IEnumerable<Planet[]>> planets)
+        {
+            var selectors = new Func<Planet, int>[]
+            {
+                p => p.Velocity.X,
+                p => p.Velocity.Y,
+                p => p.Velocity.Z,
+            };
+
+            var repetitions = selectors.Select(FindRepetition).ToArray();
+
+            return LCM(repetitions);
+
+            long FindRepetition(Func<Planet, int> selector)
+            {
+                var states = planets();
+
+                long index = 1;
+                foreach (var state in states)
+                {
+                    if (state.All(m => selector(m) == 0))
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+
+                return default;
+            }
+        }
+
+        static long LCM(params long[] numbers)
+        {
+            return numbers.Aggregate(LCM);
+            
+            long LCM(long a, long b)
+            {
+                return Math.Abs(a * b) / GCD(a, b);
+            }
+            
+            long GCD(long a, long b)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                return a;
             }
         }
 
