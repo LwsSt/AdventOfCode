a2cc06513061e03b9605c1e3cae89d860324ea6d
Lewis <lewis.stapleton@outlook.com>
2019-12-19 16:56:07 +0000

    2019 Day16 part 2

--- y2019/Day16/Program.cs
+++ y2019/Day16/Program.cs
@@ -8,15 +8,33 @@ namespace AOC2019.Day16
 {
     class Program
     {
-        public static void Main(string[] args)
+        public void Main(string[] args)
+        // public static void Main(string[] args)
         {
             // PrintPart1Examples();
+            // PrintPart2Examples();
             string text = File.ReadAllText(@"Day16\input.txt");
-            var input = text.ToCharArray().Select(c => c - '0').ToArray();
+            var ints = text.ToCharArray().Select(c => c - '0').ToArray();
 
-            Console.WriteLine("Calculating");
-            var output = Iterate(input);
-            Console.WriteLine("Result: {0}", string.Join("", output.Take(8)));
+            int offset = int.Parse(text.Substring(0, 7));
+            int[] input = new int[ints.Length * 10_000];
+            for (int i = 0; i < 10_000; i++)
+            {
+                Array.Copy(ints, 0, input, ints.Length * i, ints.Length);
+            }
+
+            Console.WriteLine("Calculating...");
+            Iterate(input);
+
+            for (int i = offset; i < offset + 8; i++)
+            {
+                Console.Write(input[i]);
+            }
+            Console.WriteLine();
+
+            // Console.WriteLine("Calculating");
+            // var output = Iterate(input);
+            // Console.WriteLine("Result: {0}", string.Join("", output.Take(8)));
         }
 
         static void PrintPart1Examples()
@@ -31,32 +49,59 @@ namespace AOC2019.Day16
             foreach (var test in testInput)
             {
                 var input = test.ToCharArray().Select(c => c - '0').ToArray();
-                Console.WriteLine("Input {0} Output {1}", test, string.Join("", Iterate(input).Take(8)));
+                Iterate(input);
+                Console.WriteLine("Input {0} Output {1}", test, string.Join("", input.Take(8)));
             }
         }
 
-        static int[] Iterate(int[] input)
+        static void PrintPart2Examples()
+        {
+            var testInput = new[]
+            {
+                "03036732577212944063491565474664",
+                "02935109699940807407585447034323",
+                "03081770884921959731165446850517",
+            };
+
+            foreach (var test in testInput)
+            {
+                int[] ints = test.Select(c => c - '0').ToArray();
+                int offset = int.Parse(test.Substring(0, 7));
+                int[] input = new int[ints.Length * 10_000];
+                for (int i = 0; i < 10_000; i++)
+                {
+                    Array.Copy(ints, 0, input, ints.Length * i, ints.Length);
+                }
+
+                Console.WriteLine("Calculating...");
+                Iterate(input);
+
+                for (int i = offset; i < offset + 8; i++)
+                {
+                    Console.Write(input[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Iterate(int[] input)
         {
             for (int i = 0; i < 100; i++)
             {
-                input = CalculateNextStep(input);
+                CalculateNextStep(input);
             }
-
-            return input;
         }
 
-        static int[] CalculateNextStep(int[] numbers)
+        static void CalculateNextStep(int[] input)
         {
-            return CalculateNextStepImpl().ToArray();
-
-            IEnumerable<int> CalculateNextStepImpl()
+            for (int i = input.Length - 1; i >= 0; i--)
             {
-                for (int i = 0; i < numbers.Length; i++)
+                if (i == input.Length - 1)
                 {
-                    IEnumerable<int> pattern = GetPattern(i);
-                    int sum = numbers.Zip(pattern, (a, b) => a * b).Sum();
-                    yield return Math.Abs(sum) % 10;
+                    continue;
                 }
+                
+                input[i] = (input[i] + input[i + 1]) % 10;
             }
         }
 
