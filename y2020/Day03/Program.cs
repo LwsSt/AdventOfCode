25af71e1b333aa9ec7109bbecbb751481a67349d
Lewis Stapleton <lewis.stapleton@runpath.com>
2020-12-03 10:23:26 +0000

    2020 Day03 Part 2

--- y2020/Day03/Program.cs
+++ y2020/Day03/Program.cs
@@ -12,6 +12,7 @@ namespace AOC2020.Day03
         {
             var map = ParseInput();
             Part1(map);
+            Part2(map);
         }
 
         public static void Part1(Space[,] map)
@@ -19,6 +20,27 @@ namespace AOC2020.Day03
             int trees = TraverseSlope(map, 1, 3);
 
             Console.WriteLine(trees);
+        }
+
+        public static void Part2(Space[,] map)
+        {
+            int total = 1;
+            var slopes = new[]
+            {
+                (heightDiff: 1, widthDiff: 1),
+                (heightDiff: 1, widthDiff: 3),
+                (heightDiff: 1, widthDiff: 5),
+                (heightDiff: 1, widthDiff: 7),
+                (heightDiff: 2, widthDiff: 1),
+            };
+
+            foreach (var (heightDiff, widthDiff) in slopes)
+            {
+                int trees = TraverseSlope(map, heightDiff, widthDiff);
+                total *= trees;
+            }
+
+            Console.WriteLine(total);
         }
 
         public static int TraverseSlope(Space[,] map, int heigthDiff, int widthDiff)
